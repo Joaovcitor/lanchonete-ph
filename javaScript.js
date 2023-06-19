e08a5570f8701ccf17d991a031403c7fd6bd3a95
João Victor <Neguchos96@gmail.com>
2023-06-19 15:27:18 -0300

    projetoPh

---
+++ javaScript.js
@@ -0,0 +1,106 @@
+// BOLO FOFO COMUM
+
+function geraMensagem(produto) {
+  return "Olá, eu gostaria de um " + produto;
+}
+function geraMensagem(produto) {
+  return "Olá, eu gostaria de " + produto;
+}
+// MISTO
+
+document.querySelector("#bnt1").addEventListener("click", function () {
+  let burguer = "misto quente!";
+  let msg = geraMensagem(burguer);
+  let link = "https://wa.me/5511999166560/?text=" + encodeURIComponent(msg);
+  window.open(link);
+});
+
+// XBURGUER
+
+document.querySelector("#bnt2").addEventListener("click", function () {
+  let burguer = "X-BURGUER!";
+  let msg = geraMensagem(burguer);
+  let link = "https://wa.me/5511999166560/?text=" + encodeURIComponent(msg);
+  window.open(link);
+});
+
+// BAURU
+
+document.querySelector("#bnt3").addEventListener("click", function () {
+  let burguer = "Bauru!";
+  let msg = geraMensagem(burguer);
+  let link = "https://wa.me/5511999166560/?text=" + encodeURIComponent(msg);
+  window.open(link);
+});
+
+// AMERICANO
+
+document.querySelector("#bnt4").addEventListener("click", function () {
+  let burguer = "Americano!";
+  let msg = geraMensagem(burguer);
+  let link = "https://wa.me/5511999166560/?text=" + encodeURIComponent(msg);
+  window.open(link);
+});
+
+// X-SALADA
+
+document.querySelector("#bnt5").addEventListener("click", function () {
+  let burguer = "um X-Salada!";
+  let msg = geraMensagem(burguer);
+  let link = "https://wa.me/5511999166560/?text=" + encodeURIComponent(msg);
+  window.open(link);
+});
+
+// X-EGGS
+
+document.querySelector("#bnt6").addEventListener("click", function () {
+  let burguer = "um X-Eggs!";
+  let msg = geraMensagem(burguer);
+  let link = "https://wa.me/5511999166560/?text=" + encodeURIComponent(msg);
+  window.open(link);
+});
+
+// X-CALABRESA
+
+document.querySelector("#bnt7").addEventListener("click", function () {
+  let burguer = "um X-Calabresa!";
+  let msg = geraMensagem(burguer);
+  let link = "https://wa.me/5511999166560/?text=" + encodeURIComponent(msg);
+  window.open(link);
+});
+
+// X-bacon
+
+document.querySelector("#bnt8").addEventListener("click", function () {
+  let burguer = "um X-Bacon!";
+  let msg = geraMensagem(burguer);
+  let link = "https://wa.me/5511999166560/?text=" + encodeURIComponent(msg);
+  window.open(link);
+});
+
+// X-ESPECIAL
+
+document.querySelector("#bnt9").addEventListener("click", function () {
+  let burguer = "um X-Especial!";
+  let msg = geraMensagem(burguer);
+  let link = "https://wa.me/5511999166560/?text=" + encodeURIComponent(msg);
+  window.open(link);
+});
+
+// X-TUDO
+
+document.querySelector("#bnt10").addEventListener("click", function () {
+  let burguer = "um X-Tudo!";
+  let msg = geraMensagem(burguer);
+  let link = "https://wa.me/5511999166560/?text=" + encodeURIComponent(msg);
+  window.open(link);
+});
+
+// X-TUDAO
+
+document.querySelector("#bnt11").addEventListener("click", function () {
+  let burguer = "um X-Tudão!";
+  let msg = geraMensagem(burguer);
+  let link = "https://wa.me/5511999166560/?text=" + encodeURIComponent(msg);
+  window.open(link);
+});
